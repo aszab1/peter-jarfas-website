ded5640f52f3a0b4ba0fe2446014535d1ebd6de5
aszab1 <adry_szabo@yahoo.co.uk>
2024-08-01 23:32:30 +0200

    added basic setup with logo in Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+
+import logo from '../assets/images/logo.png'
+
+export default function Home(){
+
+  return(
+    <>
+    <img className='logo' src={logo} alt={`A picture of `}/>
+      <h1>Tucc Tucc Tucc uszomester vagyok</h1>
+      <h1>Üdvözlet</h1>
+      <p>Ez egy példa a magyar betűk használatára: Á, É, Í, Ó, Ö, Ő, Ú, Ü, Ű</p>
+    
+    </>
+
+  )
+}
