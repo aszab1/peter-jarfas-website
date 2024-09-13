9b5223574b24a9b1cd9a1afd1a2cb03de2bf26bc
aszab1 <adry_szabo@yahoo.co.uk>
2024-09-13 14:45:39 +0100

    continued styling Photos component

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -10,11 +10,12 @@ import grouppic3 from '../assets/images/grouppic3.jpg'
 import swimlesson from '../assets/images/uszasoktatas.jpg'
 import negyen from '../assets/images/negyen.jpg'
 import fejes from '../assets/images/fejes-ugras.jpeg'
-import seniorGroupPic from '../assets/images/senior-group.jpeg'
+import seniorGroupPic from '../assets/images/senior-group.jpg'
 
 
 const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`
 const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`
+
 
 export default function Photos() {
 
@@ -48,7 +49,7 @@ export default function Photos() {
 
 
   return (
-    <div style={{ backgroundColor: '#82C4FF' }}>
+    <>
 
       <div className='swim-div'>
       <p>{t("swimLesson")}</p>
@@ -59,16 +60,17 @@ export default function Photos() {
           initial={false}
           animate={
             isLoaded && swimLessonInView
-              ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
-              : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+              ? { WebkitMaskImage: visibleMask, maskImage: visibleMask, boxShadow: '0 15px 30px rgba(0, 0, 0, 0.8)'}
+              : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask, boxShadow: 'none',}
+              
           }
           transition={{ duration: 1.5, delay: 0.2 }}
           viewport={{ once: true }}
           onLoad={() => setIsLoaded(true)}
         />
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" fill="#82C4FF"><rect fill="#60a6f0" width="100%" height="100%" />
-        <path d="M0 0v90.2C49.7 99.9 105 82 160 65c75.5-23.3 145.5-22.4 222-3 63 16 119 14 173-8 79.5-32.4 156.2-27.6 240-10 82.6 17.4 143-1 205-31.7V0H0Z"></path>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none" style={{ display: 'block', margin: 0, padding: 0, border: 'none' }} ><rect fill="#60a6f0" width="100%" height="100%" stroke="none" />
+        <path d="M0 0v90.2C49.7 99.9 105 82 160 65c75.5-23.3 145.5-22.4 222-3 63 16 119 14 173-8 79.5-32.4 156.2-27.6 240-10 82.6 17.4 143-1 205-31.7V0H0Z" fill="#82C4FF" stroke="none"></path>
       </svg>
       <div
         className='uszoda-div' style={{ backgroundColor: '#60a6f0' }}
@@ -120,10 +122,10 @@ export default function Photos() {
           <path d="M409.602 341.336c0 37.7-30.567 68.266-68.266 68.266-37.703 0-68.266-30.567-68.266-68.266 0-37.703 30.563-68.266 68.266-68.266 37.7 0 68.266 30.563 68.266 68.266zM324.27 179.2c0 32.991-26.743 59.734-59.735 59.734-32.992 0-59.734-26.743-59.734-59.735 0-32.988 26.742-59.734 59.734-59.734 32.992 0 59.735 26.746 59.735 59.734zm-119.47 128c0 56.554-45.847 102.402-102.402 102.402C45.848 409.602 0 363.754 0 307.199c0-56.554 45.848-102.398 102.398-102.398 56.555 0 102.403 45.844 102.403 102.398zM170.672 85.336c0 47.129-38.207 85.336-85.336 85.336S0 132.465 0 85.336 38.207 0 85.336 0s85.336 38.207 85.336 85.336zM409.602 51.2c0 28.277-22.926 51.198-51.204 51.198-28.277 0-51.199-22.921-51.199-51.199C307.2 22.922 330.121 0 358.4 0c28.277 0 51.203 22.922 51.203 51.2zm-17.071 170.663c0 9.426-7.64 17.067-17.066 17.067-9.426 0-17.067-7.64-17.067-17.067 0-9.422 7.641-17.062 17.067-17.062s17.066 7.64 17.066 17.062zM264.535 59.734c0 14.141-11.46 25.602-25.601 25.602-14.137 0-25.598-11.461-25.598-25.602 0-14.136 11.46-25.597 25.598-25.597 14.14 0 25.601 11.46 25.601 25.597zm0 0" fill="url(&quot;#SvgjsLinearGradient1042&quot;)"></path><defs><linearGradient id="SvgjsLinearGradient1042"><stop stopColor="#d5dee7" offset="0"></stop><stop stopColor="#ffffff" offset="1"></stop></linearGradient></defs></svg>
         </div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" fill="#60a6f0"><rect fill="#4d9aeb" width="100%" height="100%" />
-        <path d="M0 0v90.2C49.7 99.9 105 82 160 65c75.5-23.3 145.5-22.4 222-3 63 16 119 14 173-8 79.5-32.4 156.2-27.6 240-10 82.6 17.4 143-1 205-31.7V0H0Z"></path>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none" style={{ display: 'block', margin: 0, padding: 0, border: 'none' }}><rect fill="#4d9aeb" width="100%" height="100%" stroke="none" />
+        <path d="M0 0v90.2C49.7 99.9 105 82 160 65c75.5-23.3 145.5-22.4 222-3 63 16 119 14 173-8 79.5-32.4 156.2-27.6 240-10 82.6 17.4 143-1 205-31.7V0H0Z" fill='#60a6f0' stroke="none"></path>
       </svg>
-      <div style={{backgroundColor: '#4d9aeb'}}>
+      <div style={{backgroundColor: '#4d9aeb', stroke: 'none'}}>
       <div className='girls'>
         <motion.img ref={girlsRef} className='pic1' 
           src={pic1} alt="with one of the student"
@@ -180,8 +182,8 @@ export default function Photos() {
         />
       </div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" fill="#4d9aeb"><rect fill="#2384eb" width="100%" height="100%" />
-        <path d="M0 0v90.2C49.7 99.9 105 82 160 65c75.5-23.3 145.5-22.4 222-3 63 16 119 14 173-8 79.5-32.4 156.2-27.6 240-10 82.6 17.4 143-1 205-31.7V0H0Z"></path>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none"  style={{ display: 'block', margin: 0, padding: 0, border: 'none' }}><rect fill="#2384eb" width="100%" height="100%" stroke="none"/>
+        <path d="M0 0v90.2C49.7 99.9 105 82 160 65c75.5-23.3 145.5-22.4 222-3 63 16 119 14 173-8 79.5-32.4 156.2-27.6 240-10 82.6 17.4 143-1 205-31.7V0H0Z" fill="#4d9aeb" stroke="none"></path>
       </svg>
       
       <div className='senior-div' style={{ backgroundColor: '#2384eb' }}>
@@ -228,10 +230,12 @@ export default function Photos() {
           <path d="M409.602 341.336c0 37.7-30.567 68.266-68.266 68.266-37.703 0-68.266-30.567-68.266-68.266 0-37.703 30.563-68.266 68.266-68.266 37.7 0 68.266 30.563 68.266 68.266zM324.27 179.2c0 32.991-26.743 59.734-59.735 59.734-32.992 0-59.734-26.743-59.734-59.735 0-32.988 26.742-59.734 59.734-59.734 32.992 0 59.735 26.746 59.735 59.734zm-119.47 128c0 56.554-45.847 102.402-102.402 102.402C45.848 409.602 0 363.754 0 307.199c0-56.554 45.848-102.398 102.398-102.398 56.555 0 102.403 45.844 102.403 102.398zM170.672 85.336c0 47.129-38.207 85.336-85.336 85.336S0 132.465 0 85.336 38.207 0 85.336 0s85.336 38.207 85.336 85.336zM409.602 51.2c0 28.277-22.926 51.198-51.204 51.198-28.277 0-51.199-22.921-51.199-51.199C307.2 22.922 330.121 0 358.4 0c28.277 0 51.203 22.922 51.203 51.2zm-17.071 170.663c0 9.426-7.64 17.067-17.066 17.067-9.426 0-17.067-7.64-17.067-17.067 0-9.422 7.641-17.062 17.067-17.062s17.066 7.64 17.066 17.062zM264.535 59.734c0 14.141-11.46 25.602-25.601 25.602-14.137 0-25.598-11.461-25.598-25.602 0-14.136 11.46-25.597 25.598-25.597 14.14 0 25.601 11.46 25.601 25.597zm0 0" fill="url(&quot;#SvgjsLinearGradient1042&quot;)"></path><defs><linearGradient id="SvgjsLinearGradient1042"><stop stopColor="#d5dee7" offset="0"></stop><stop stopColor="#ffffff" offset="1"></stop></linearGradient></defs></svg>
         </div>
       </div>
-
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none"  style={{ display: 'block', margin: 0, padding: 0, border: 'none' }}><rect fill="#2384eb" width="100%" height="1%" stroke="none"/>
+        <path d="M0 0v90.2C49.7 99.9 105 82 160 65c75.5-23.3 145.5-22.4 222-3 63 16 119 14 173-8 79.5-32.4 156.2-27.6 240-10 82.6 17.4 143-1 205-31.7V0H0Z" fill="#2384eb" stroke="none"></path>
+      </svg>
       <button className="back-button" onClick={handleBackClick}><Icon className='back-button' icon='ep:back' /></button>
 
-    </div>
+    </>
 
   )
 }
